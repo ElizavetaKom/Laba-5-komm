d3e4551a3e50113f1ddfd939ab74caf455f05c1f
Elizaveta Komendanchenko <54947624+ElizavetaKom@users.noreply.github.com>
2019-12-12 23:18:08 +0300

    Add files via upload

---
+++ lab5komm/src/Dish/TesterDish.java
@@ -0,0 +1,8 @@
+package Dish;
+
+public class TesterDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("Стекло", "Зеленый", "Средний");
+        System.out.println(t);
+    }
+}
